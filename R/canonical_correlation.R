b6a03ba8c06cdc209c37e60be69eda307a5ce70a
sevvandi <sevvandi.kandanaarachchi@gmail.com>
2022-11-07 13:58:57 +1100

    update cannonical correlation

--- R/canonical_correlation.R
+++ R/canonical_correlation.R
@@ -1,21 +1,18 @@
-#' Plots transformed variables from Canonical Correlation Analysis
+#' Computes transformed variables from Canonical Correlation Analysis
 #'
-#' Performs Canonical Correlation Analysis (CCA) using 2 datasets and plots the first CC function of both datasets.
+#' Computes Canonical Correlation Analysis (CCA) using 2 datasets.
 #'
 #' @param df1 The first dataset.
 #' @param df2 The second dataset. The dimensions of both datasets need to be the same.
-#' @param xlab The xlabel to appear on CCA plot.
 #'
 #' @examples
 #' df1 <- SSTdatashort[1:100, ]
 #' df2 <- SSTdatashort[401:500, ]
-#' st_cancor(df1, df2, xlab = "Longitude")
+#' canonical_correlation(df1, df2)
 #'
 #' @importFrom stats cancor
-#' @export st_cancor
-st_cancor <- function(df1,
-                      df2,
-                      xlab = "Year"){
+#' @export canonical_correlation
+canonical_correlation <- function(df1, df2){
   # option 1 - using 2 separate datasets - using raw
   if(missing(df1)){
     stop("Empty dataframe df1. Please give a dataframe with N1 rows and N2 columns.")
@@ -34,7 +31,7 @@ st_cancor <- function(df1,
   nn <- dim(df1)[1]
   dd <- dim(df1)[2]
   if(nn < dd){
-    print('Using the transpose to perform CCA. Consider changing xlab.')
+    print('Rows less than columns. Using the transpose to perform CCA')
     df1 <- t(df1)
     df2 <- t(df2)
   }
@@ -50,13 +47,11 @@ st_cancor <- function(df1,
                         Variable = c(rep("CCA1-df1", nn), rep("CCA1-df2", nn)))
 
 
-  # Now plot
-  sst_cca <- ggplot(CCA_df, aes(x=t, y=ts, colour = Variable)) + geom_line(size = 0.8) +
-    scale_color_manual(values = c("dark blue", "dark red")) +
-    ylab("CCA  variables")  +
-    xlab(xlab)  +
-    theme_bw() +
-    ggtitle("Canonical Correlation Analysis")
+  structure(list(
+    cancor_df = CCA_df,
+    df1 = df1,
+    df2 = df2,
+    call = match.call()
+  ), class='cancor')
 
-  sst_cca
 }
