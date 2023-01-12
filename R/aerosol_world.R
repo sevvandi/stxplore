597641b0ebc080da1fdb6fec9097b95fb525af52
sevvandi <sevvandi.kandanaarachchi@gmail.com>
2023-01-12 13:51:40 +1100

    add 2 star objects

---
+++ R/aerosol_world.R
@@ -0,0 +1,7 @@
+#' Data from of NASA Earth Observations at https://neo.gsfc.nasa.gov
+#'
+#' Aerosol optical thickness data from December 2019 to December 2020, taken monthly.
+#'
+#' @format A stars object with x, y and time containing aerosol thickness. Dimensions 360x180x13
+#'
+"aerosol_world"
