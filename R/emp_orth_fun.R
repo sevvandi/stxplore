ae9ea3b6230557003b88f557e606bff001821055
sevvandi <sevvandi.kandanaarachchi@gmail.com>
2023-01-23 17:17:18 +1100

    combine emp_orth_fun to one file

--- R/emp_orth_fun.R
+++ R/emp_orth_fun.R
@@ -1,19 +1,51 @@
-#' Computes empirical orthogonal functions using a dataframe
+#' Computes empirical orthogonal functions using a dataframe or a stars object.
 #'
-#' Computes empirical orthogonal functions of the data.
+#' Computes empirical orthogonal functions of the data. Function autoplot can plot the output.
 #'
-#' @inheritParams emp_orth_fun
 #' @inheritParams semivariogram.data.frame
+#' @inheritParams spatial_snapshots
+#' @param object For autoplot: the output of the function `emp_orth_fun'.
+#' @param EOF_num For autoplot: the number of Empirical Orthogonal Functions (EOFs) to plot.
+#' @param only_EOF For autoplot: if \code{TRUE}, only the spatial EOF function would be plotted.
+#' @param only_TS For autoplot: if \code{TRUE}, only the PC time series would be plotted. If both are set to \code{FALSE},
+#'                 both plots would be displayed. Both cannot be set to \code{TRUE}.
+#' @param ... Other arguments currently ignored.
+#'
+#' @returns An emporthfun object with temporal PCs and spatial EOFs.
+#'
 #' @examples
+#' # dataframe example
 #' data(SSTlonlatshort)
 #' data(SSTdatashort)
 #' data(SSTlandmaskshort)
-#' # Take first 396 months (33 years) and delete land
+# Take first 396 months (33 years) and delete land
 #' delete_rows <- which(SSTlandmaskshort  ==  1)
 #' SSTdatashort   <- SSTdatashort[-delete_rows, 1:396]
-#' emp_orth_fun(SSTlonlatshort[-delete_rows,  ],
-#'              SSTdatashort)
+#' emp1 <- emp_orth_fun(SSTlonlatshort[-delete_rows,  ],
+#'                      SSTdatashort)
+#' autoplot(emp1,
+#'          EOF_num = 1)
 #'
+#'
+#' # stars example
+#' library(dplyr)
+#' library(stars)
+#' # Create a stars object from a data frame
+#' precip_df <- NOAA_df_1990[NOAA_df_1990$proc == 'Precip', ] %>%
+#'   filter(date >= "1992-02-01" & date <= "1992-02-05")
+#' precip <- precip_df[ ,c('lat', 'lon', 'date', 'z')]
+#' st_precip <- st_as_stars(precip, dims = c("lon", "lat", "date"))
+#' emp <- emp_orth_fun(st_precip)
+#' autoplot(emp, only_TS = TRUE)
+#'
+#' @export
+emp_orth_fun <- function(x,
+                         ...){
+  UseMethod("emp_orth_fun")
+}
+
+
+#' @rdname emp_orth_fun
 #' @export
 emp_orth_fun.data.frame <- function(x,
                                     values_df,
@@ -70,21 +102,8 @@ emp_orth_fun.data.frame <- function(x,
 
 }
 
-#' Computes empirical orthogonal functions for a stars object
-#'
-#' Computes empirical orthogonal functions of the data.
-#'
-#' @inheritParams emp_orth_fun
-#' @examples
-#' library(dplyr)
-#' library(stars)
-#' # Create a stars object from a data frame
-#' precip_df <- NOAA_df_1990[NOAA_df_1990$proc == 'Precip', ] %>%
-#'   filter(date >= "1992-02-01" & date <= "1992-02-28")
-#' precip <- precip_df[ ,c('lat', 'lon', 'date', 'z')]
-#' st_precip <- st_as_stars(precip, dims = c("lon", "lat", "date"))
-#' emp_orth_fun(st_precip)
-#'
+
+#' @rdname emp_orth_fun
 #' @export
 emp_orth_fun.stars <- function(x,
                                ...){
@@ -115,4 +134,48 @@ emp_orth_fun.stars <- function(x,
 
   emp_orth_fun.data.frame(locations_df2,
                           values_df2)
+}
+
+
+#' @rdname emp_orth_fun
+#' @export
+#' @export
+autoplot.emporthfun <- function(object,
+                                EOF_num = 1,
+                                palette = "Spectral",
+                                only_EOF = FALSE,
+                                only_TS = FALSE,
+                                ...){
+  EOF <- NULL
+  EOFs <- object$eofs
+  TS <- object$pcts
+  nPC <- object$pcts$nPC
+
+  variable <- paste("EOF",EOF_num, sep="")
+  lon <- 'lon'
+  lat <- 'lat'
+  # Plot EOF
+  eof_p <- ggplot(EOFs)  +  geom_tile(ggplot2::aes_string(x = lon, y = lat, fill = variable))  +
+    scale_fill_distiller(palette = palette, guide = "colourbar") +
+    theme_bw()  +
+    xlab(expression(paste("Longitude", degree)))  +
+    ylab(expression(paste("Latitude", degree))) +
+    ggtitle(paste("EOF_",EOF_num, sep=""))
+
+  # Plot PC of that EOF
+  varriable2 <- paste("X", EOF_num, sep="")
+  TS_EOF <- subset(TS, EOF == varriable2)
+  pcts_eof <- ggplot(TS_EOF, aes(x=t, y=nPC)) + geom_line(size = 0.8) +
+    xlab("Time (t)") +
+    ylab("Normalized Principal Component") +
+    ggtitle(paste('PC Time series for EOF_', EOF_num, sep=""))
+
+  if(only_EOF){
+    eof_p
+  }else if(only_TS){
+    pcts_eof
+  }else{
+    gridExtra::grid.arrange(eof_p, pcts_eof)
+  }
+
 }
