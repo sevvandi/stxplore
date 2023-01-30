06a5e8e409562c7881f240ca0d1e03aae27344ea
sevvandi <sevvandi.kandanaarachchi@gmail.com>
2023-01-30 16:45:44 +1100

    combine hovmoller to one file

--- R/temporal_means.R
+++ R/temporal_means.R
@@ -1,11 +1,18 @@
-#' Computes temporal empirical means using a dataframe as input
+#' Computes temporal empirical means using a dataframe or a stars object.
 #'
+#' @description
 #' This function computes temporal empirical means averaged per time unit.
+#' This function can take either a stars object or a dataframe. Input arguments
+#' differ for each case. The function autoplot plots the output.
 #'
 #' @inheritParams temporal_snapshots.data.frame
 #' @inheritParams spatial_snapshots.data.frame
+#' @inheritParams temporal_snapshots
+#' @param object For autoplot: the output of the function `temporal_means'.
+#' @param legend_title For autoplot: the title for the legend.
 #'
 #' @examples
+#' # dataframe example
 #' data(NOAA_df_1990)
 #' library(dplyr)
 #' Tmax <- filter(NOAA_df_1990,                      # subset the data
@@ -13,10 +20,27 @@
 #'                 month %in% 5:9 &                 # May to July
 #'                 year == 1993)                    # year 1993
 #' Tmax$t <- Tmax$julian - min(Tmax$julian) + 1      # create a new time variable starting at 1
-#' temporal_means(Tmax,
+#' tem <- temporal_means(Tmax,
 #'        t_col = 'date',
 #'        z_col = 'z',
 #'        id_col = 'id')
+#'autoplot(tem)
+#'
+#' # stars example
+#' library(stars)
+#' library(dplyr)
+#' library(units)
+#' # Example
+#' prec_file = system.file("nc/test_stageiv_xyt.nc", package = "stars")
+#' prec <- read_ncdf(prec_file)
+#' temporal_means(prec)
+#' @export
+temporal_means <- function(x,
+                           ...){
+  UseMethod("temporal_means")
+}
+
+#' @rdname temporal_means
 #' @export
 temporal_means.data.frame <- function(x,
                                       t_col,
@@ -59,21 +83,7 @@ temporal_means.data.frame <- function(x,
 }
 
 
-#' Computes temporal empirical means using a stars object as input
-#'
-#' This function computes temporal empirical means averaged per time unit.
-#'
-#' @inheritParams temporal_snapshots.data.frame
-#' @inheritParams spatial_snapshots.data.frame
-#'
-#' @examples
-#' library(stars)
-#' library(dplyr)
-#' library(units)
-#' # Example
-#' prec_file = system.file("nc/test_stageiv_xyt.nc", package = "stars")
-#' prec <- read_ncdf(prec_file)
-#' temporal_means(prec)
+#' @rdname temporal_means
 #' @export
 temporal_means.stars <- function(x,
                                  ...){
@@ -104,5 +114,49 @@ temporal_means.stars <- function(x,
     averages = df_av,
     call = match.call()
   ), class='temporalmeans')
+}
+
+#' @rdname temporal_means
+#' @export
+autoplot.temporalmeans <- function(object,
+                                   ylab = "Value",
+                                   xlab ="Time",
+                                   legend_title ="",
+                                   title = "Temporal Empirical Means",
+                                   ...){
+
+  df <- as.data.frame(object$data)
+  df_av <- as.data.frame(object$averages)
+
+  num_obs <- dim(df)[1]
+  if(num_obs > 50000){  # More than 50000 data points
+    set.seed(1)
+    ss <- sample(num_obs, 50000)
+    df <- df[ss, ]
+  }
+
+  meanz <- time <- NULL
+
+  p <- ggplot() +
+    geom_line(data = df,
+              aes(x = df[ ,2],
+                  y = df[ ,1],
+                  group = df[ ,3],
+                  colour = "blue"),
+              alpha = 0.04) +
+    geom_line(data = df_av,
+              aes(x = time,
+                  y = meanz,
+                  colour = "black")) +
+    xlab(xlab) +
+    ylab(ylab) +
+    theme_bw() +
+    scale_color_manual(name = legend_title,
+                       values = c(blue = "blue", black = "black"),
+                       labels = c("Observed", "Average"),
+                       guide = "legend") +
+    ggtitle(title)
+  p
+
 }
 
